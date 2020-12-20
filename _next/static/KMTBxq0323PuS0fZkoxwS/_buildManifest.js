8e5e2d6078a585eb2090fa21f1f5ad9dbf87f232
Atreya Bain <christophervrose@gmail.com>
2020-12-20 11:48:29 +0530

    Updates

---
+++ _next/static/KMTBxq0323PuS0fZkoxwS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F6cd516ab50b26e63d9d2.css","static\u002Fchunks\u002Fpages\u002Findex-7a1ac31a4db9c130f47b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bcbd94eeb8b76941575c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
